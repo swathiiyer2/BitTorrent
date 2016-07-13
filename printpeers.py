c3543e67f9e6cb6df8d27b4aea766599d6abc783
Taylor Burgess <tayloraburgess@gmail.com>
2016-07-13 14:34:06 -0400

    Generate peer IP addresses & ports and store in array

--- printpeers.py
+++ printpeers.py
@@ -1,10 +1,14 @@
-import decoder, ipaddress
+import decoder
+import requests
 
-binstring = decoder.testTorrent.peers
-print(binstring)
+bin_string = decoder.testTorrent.peers
 
-peer_dict = {}
+peer_array = []
 
-for n in range(0,len(binstring), 6):
-    newIP = str(binstring[n]) + '.' + str(binstring[n+1])+'.'+str(binstring[n+2])+'.'+str(binstring[n+3])
-    print(newIP)
+for n in range(0,len(bin_string), 6):
+    array_string = ""
+    array_string += str(bin_string[n]) + '.' + str(bin_string[n+1])+'.'+str(bin_string[n+2])+'.'+str(bin_string[n+3])
+    array_string += ":" + str(bin_string[n + 4] * 256 + bin_string[n + 5])
+    peer_array.append(array_string)
+
+print(peer_array)
