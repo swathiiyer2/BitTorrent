c17fbf0621a08e442befc92e032e2658667eb4b9
John <jjkiely@gmail.com>
2016-07-12 21:49:30 -0400

    getting response from tracker, works with python 3

--- decoder.py
+++ decoder.py
@@ -1,46 +1,50 @@
-from bencode import bencode, bdecode
-import requests
+from bcoding import bencode, bdecode
 from pprint import pprint
 from hashlib import sha1
+from string import ascii_lowercase, digits
+from random import choice
+import requests
+
 
 class decodeFile():
+  def __init__(self, inputURL):
+    self.inputURL = inputURL
+    self.decodeFile()
+    self.tracker()
 
-	def __init__(self, inputURL):
-		self.inputURL = inputURL
-		self.decodeFile()
-		self.tracker()
+  def decodeFile(self):
+    f = open(self.inputURL, "rb")
+    d = bdecode(f.read())
+    self.url = d['announce']
+    self.info = d['info']
+    self.reencoded = bencode(self.info)
 
-	def decodeFile(self):
-		f = open(self.inputURL, "rb")
-		d = bdecode(f.read())
-		#print d
-		#print d['announce']
-		self.url = d['announce']
-		self.info = d['info']
-		#print ("self.info", self.info)
-		self.reencoded = bencode(self.info)
-		#print "+++++++++++++++"
-		#print self.reencoded
-		#print "+++++++++++++++"
+  def tracker(self):
+    info_hash = sha1(self.reencoded).digest()
+    peer_id = self.make_id()
+    port = "9999"
+    uploaded = "0"
+    downloaded = "0"
+    left = str(self.info['files'][1]['length'])
+    request_hash = {
+      "info_hash": info_hash,
+      "peer_id": peer_id,
+      "port": port,
+      "uploaded": uploaded,
+      "downloaded": downloaded,
+      "left": left}
+    r = requests.get(self.url, params=request_hash)
 
-	def tracker(self):
-		info_hash = str(sha1(self.reencoded))
-		peer_id = "-UM1870-%92%a5Y%04e_%22%d2w%1a%87%23"
-		port = "9999"
-		uploaded = "0"
-		downloaded = "0"
-		left = str(self.info['files'][1]['length'])
-		requestString = (self.url + 
-			'?info_hash=' + info_hash + 
-			"&peer_id=" + peer_id + 
-			"&port=" + port + 
-			"&uploaded=" + uploaded +
-			"&downloaded=" + downloaded +
-			"&left=" + left)
-		print ("rq", requestString)
-		r = requests.get(requestString)
+    print(r.content)
 
-		print r.status_code
+  def make_id(self):
+    id = "RC0001"
+    for _ in range(14):
+      id+= choice(list(ascii_lowercase + digits))
+    return id
+    
+
+      
 
 testTorrent = decodeFile("Marcus Williams.Miles.Davis.Marcus.Miller.Live.In.Paris.[mp3_192k].[www.mywpmusic.com] [mininova].torrent")
 
