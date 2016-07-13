1f5f69d9a5472388db888678d1483f6c0cb0dba1
Taylor Burgess <tayloraburgess@gmail.com>
2016-07-13 13:47:55 -0400

    Decode peer IP Addresses and store in objects

--- printpeers.py
+++ printpeers.py
@@ -1,3 +1,10 @@
-import decoder
+import decoder, ipaddress
 
-print(testTorrent.peers)
+binstring = decoder.testTorrent.peers
+print(binstring)
+
+peer_dict = {}
+
+for n in range(0,len(binstring), 6):
+    newIP = str(binstring[n]) + '.' + str(binstring[n+1])+'.'+str(binstring[n+2])+'.'+str(binstring[n+3])
+    print(newIP)
