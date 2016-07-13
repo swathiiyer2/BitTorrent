97d0fea00171243b819902d87ad876aea8193a64
John <jjkiely@gmail.com>
2016-07-13 12:09:46 -0400

    decoder now creates torrent object

---
+++ torrent.py
@@ -0,0 +1,12 @@
+
+
+class Torrent():
+
+  def __init__(self, info):
+    self.uploaded = 0
+    self.downloaded = 0
+    self.length = info['files'][1]['length']
+    self.left = self.length - self.downloaded
+
+  
+    
