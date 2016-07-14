8838b30f3fcdf4924532ce2603ae79b2e6cfbb8e
naomi <naomi@aktivix.org>
2016-07-14 17:53:32 -0400

    more output from printpeers

--- printpeers.py
+++ printpeers.py
@@ -3,7 +3,10 @@ import connect
 
 decoded = decoder.decodeFile("Marcus Williams.Miles.Davis.Marcus.Miller.Live.In.Paris.[mp3_192k].[www.mywpmusic.com] [mininova].torrent")
 tracker = connect.Tracker(decoded.url, decoded.info_hash, decoded.left)
-print(tracker.tracker())
+print("Peers: " + repr(tracker.tracker()))
+print("info_hash: " + repr(decoded.info_hash))
+print("peer_id: " + tracker.peer_id)
+print("pstr: " + "BitTorrent protocol")
 
 
 
