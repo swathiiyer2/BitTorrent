80705f87bb5c13bff7fdb1a4030b7406c63b7979
John <jjkiely@gmail.com>
2016-07-16 18:31:38 -0400

    connection to peer timing out

--- peer.py
+++ peer.py
@@ -2,8 +2,11 @@ import socket
 
 class Peer:
 
-  def __init__(self, address, info_hash, peer_id, torrent):
+  def __init__(self, address, port, info_hash, peer_id, torrent):
+    #if you pass the address and port in as a tuple the socket throws an error
+    #but when you don't combine them until the connection it seems to work
     self.address = address
+    self.port = port
     self.peer_id = peer_id
     self.torrent = torrent
     self.info_hash = info_hash
@@ -17,22 +20,20 @@ class Peer:
     self.connection = socket.socket()
     self.connection.settimeout(0.5)
     try:
-      self.connection.connect(self.address)
+      self.connection.connect((self.address, self.port))
     except socket.timeout:
       print(self.address + " timed out")
     except socket.error:
       print(self.address + " socket error")
-    except:
-      raise Exception
+    
     self.connection.send(self.handshake())
-
+    
     try:
       response = self.connection.recv(64)
       assert self.check_response(response)
     except AssertionError:
       print(self.address + " bad response")
-    except:
-      raise Exception
+    
 
   def handshake(self):
     h = b''.join([b'\x13', self.pstr.encode('ascii'), b'\x00\x00\x00\x00\x00\x00\x00\x00', self.info_hash, self.peer_id.encode('ascii')])
--- test_peers.py
+++ test_peers.py
@@ -1,13 +1,22 @@
-import peer
+from peer import Peer
 import pytest
 
 
 info_hash = b'p\xff\xeeh_\x14\x93BHd\xab\x1b\xe5I\x1b\x8f\xdc3^#'
 peer_id = "RC0001rp7nl84gjs2n1k"
 
-peer = peer.Peer("address", peer_id, info_hash)
+peer = Peer("address", "port", peer_id, info_hash, "torrent")
 
 def test_handshake():
   h = peer.handshake()
   assert h == b'\x13BitTorrent protocol\x00\x00\x00\x00\x00\x00\x00\x00p\xff\xeeh_\x14\x93BHd\xab\x1b\xe5I\x1b\x8f\xdc3^#RC0001rp7nl84gjs2n1k'
-  
+
+
+from connect import Tracker
+from test_decode import d
+
+infohash = d.info_hash
+url = d.url
+left = d.left
+
+tracker = Tracker(url, infohash, left)
